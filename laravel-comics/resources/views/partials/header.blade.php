718c107a8a32ca09fadd6d01ed6798aa5e853205
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-18 17:53:00 +0200

    header

---
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -0,0 +1,27 @@
+{{-- HEADER --}}
+<header>
+    <div class="top-bar">
+    <div class="flex wrap">
+         <a href="">DC POWER™ VISA®</a>
+         <a href="">ADDITIONAL DC SITES </a>
+    </div>
+    </div>
+    <nav class="wrap flex">
+        <div class="logo-container"><img src="{{asset('imgs/dc-logo.png')}}" alt=""></div>
+        <div class="links">
+            <a href="">CHARACTERS</a> 
+            <a href="">COMICS</a> 
+            <a href="">MOVIES</a> 
+            <a href="">TV</a> 
+            <a href="">GAMES</a> 
+            <a href="">COLLECTIBLES</a> 
+            <a href="">VIDEOS</a> 
+            <a href="">FANS</a> 
+            <a href="">NEWS</a> 
+            <a href="">SHOP</a> 
+        </div>
+        <input type="text" placeholder="Search">
+    </nav>
+</header>
+<div class="jumbotron">
+</div>
