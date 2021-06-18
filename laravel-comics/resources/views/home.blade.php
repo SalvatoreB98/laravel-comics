f9729a1568b530ce1cb344db6b52fb85cb402214
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-06-18 20:22:56 +0200

    Esercizio Ok

--- laravel-comics/resources/views/home.blade.php
+++ laravel-comics/resources/views/home.blade.php
@@ -1,2 +1,23 @@
 @extends('layout.default')
 @section('home', 'header')
+@section('content')
+<section class="content">
+    <div class="wrap">
+        <div class="title">
+            <h3>CURRENT SERIES</h3>
+        </div>
+    </div>
+
+    <div class="current wrap">
+        {{-- @dump($comicsList); --}}
+        @foreach($comicsList as $key => $comic)
+        <div class="comic-container padding-2">
+            <a href="">
+                <div class="cover"><img src="{{$comic['thumb']}}" alt=""></div>
+                <div class="description"> {{Str::limit($comic["title"],17)}}</div>
+            </a>
+        </div>
+        @endforeach
+    </div>
+</section>
+@endsection
